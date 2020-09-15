481766ab938b2a0ac9afe2d0e6e3438bb3004ac3
Yang Siyu <siyuyang1995@gmail.com>
2020-09-15 14:12:47 +0800

    [yang siyu] init project

---
+++ src/main/java/com/example/WonderCV/WonderCvApplication.java
@@ -0,0 +1,13 @@
+package com.example.WonderCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderCvApplication.class, args);
+	}
+
+}
